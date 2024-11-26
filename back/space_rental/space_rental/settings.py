ebf53c455b1fff1b4afed9ed4f5e88ef34c2a5cd
jeongui31 <jangjung31@dgu.ac.kr>
2024-11-26 10:10:20 +0900

    프론트 login 화면 연결 (입력칸 css 수정 필요)

--- back/space_rental/space_rental/settings.py
+++ back/space_rental/space_rental/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS=[BASE_DIR, "Static"]
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Adjust based on your project structure
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
